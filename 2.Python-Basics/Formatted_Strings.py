b8775707f2f22896881ffd2e01ca3e0d8be50513
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-11-06 22:51:55 +0330

    Formatted Strings in python.

---
+++ 2.Python-Basics/Formatted_Strings.py
@@ -0,0 +1,6 @@
+name = 'Johnny'
+age = 55
+#print('Hi ' + name + '. You are ' + str(age) + ' year old.')
+print(f"Hi {name}. You are {age} years old.")
+print('Hi {0}. You are {1} years old.'.format(name , age))
+print('Hi {new_name}. You are {new_age} years old.'.format(new_name='sally', new_age=100))

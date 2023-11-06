89b4fafc1638e3cb423ff3fae507296bf4dca2fb
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-11-06 22:54:24 +0330

    String Indexes in python.

---
+++ 2.Python-Basics/String_Indexes.py
@@ -0,0 +1,10 @@
+selfish = 'me me me'
+        #  01234567
+# [start:stop:stepover]
+print(selfish[1])
+print(selfish[:])
+print(selfish[1:])
+print(selfish[:5])
+print(selfish[0:7:2])
+print(selfish[-1])
+print(selfish[::-1])        

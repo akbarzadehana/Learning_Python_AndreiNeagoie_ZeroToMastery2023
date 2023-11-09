a1f9b8b456ae1937427f5a3003c1dec7263f6bc2
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-11-09 14:20:50 +0330

    Truthy and Fulsy in python

---
+++ 3.Python-Basics-II/Truthy_and_Falsy.py
@@ -0,0 +1,9 @@
+username = "Anahita"
+password = "13751375"
+# username = ''
+# password = 0
+
+if username and password:
+    print("Check the username and password to login.")
+else:
+    print("Please enter your username and password.")

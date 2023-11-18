5ea08aa4c93f63ea695fef95c571d2618337da07
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-11-18 13:08:42 +0330

    Methods V.S. Functions in python

---
+++ 3.Python-Basics-II/Methods_VS_Functions.py
@@ -0,0 +1,17 @@
+# Functions
+list((1, 2, 3))
+print("!")
+max([4, 5, 6])
+input("Enter your username and password:")
+
+
+def some_random_stuf():
+    pass
+
+
+some_random_stuf()
+
+# Method has to be owned by something
+# The string owns the method capatalize
+print("Hello".capitalize())
+# capatalize()

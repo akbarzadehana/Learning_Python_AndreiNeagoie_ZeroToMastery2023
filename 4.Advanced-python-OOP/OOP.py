771eb49db7321c9966e94e303b8cfa8ae12bc23f
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-11-21 13:44:50 +0330

    Object Oriented Programming in python

---
+++ 4.Advanced-python-OOP/OOP.py
@@ -0,0 +1,18 @@
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type("Hi"))
+print(type([]))
+print(type(()))
+print(type({}))
+
+
+# Class
+class BigObject:
+    pass
+
+
+# Instanciate
+obj1 = BigObject()
+print(type(obj1))

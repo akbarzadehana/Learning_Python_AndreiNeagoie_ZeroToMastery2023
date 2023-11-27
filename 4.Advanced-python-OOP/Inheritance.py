2ae00d48d46ea32fcc2c6e28615f4a7dc9c94fef
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-11-27 11:10:23 +0330

    Inheritance

---
+++ 4.Advanced-python-OOP/Inheritance.py
@@ -0,0 +1,34 @@
+# when we do something like User, it's actually accepting object as the parent.
+class User:
+    def sign_in(self):
+        print("logged in")
+
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f"attacking with power of {self.power}")
+
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f"attacking with arrows: arrows left- {self.num_arrows}")
+
+
+wizard1 = Wizard("Merlin", 50)
+archer1 = Archer("Robin", 100)
+wizard1.attack()
+
+# Check if something is an instance of a class
+print(isinstance(wizard1, Wizard))
+print(isinstance(wizard1, User))
+
+# Everything in python is an object.
+print(isinstance(wizard1, object))

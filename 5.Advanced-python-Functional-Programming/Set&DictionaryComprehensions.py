752ec3bb4b58dd0acf70c76a2a9044e04195fa5d
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-12-05 19:02:10 +0330

    set and dictionary comprehensions in python

---
+++ 5.Advanced-python-Functional-Programming/Set&DictionaryComprehensions.py
@@ -0,0 +1,13 @@
+# set just like lists, but only uniqe items.
+my_list = {param for param in "hello"}
+my_list2 = {num for num in range(0, 100)}
+my_list4 = {num**2 for num in range(0, 100) if num % 2 == 0}
+print(my_list)
+print(my_list2)
+
+# dictionary
+simple_dict = {"a": 1, "b": 2}
+my_dict = {k: v**2 for k, v in simple_dict.items() if v % 2 == 0}
+my_dict2 = {num: num * 2 for num in [1, 2, 3]}
+print(my_dict)
+print(my_dict2)

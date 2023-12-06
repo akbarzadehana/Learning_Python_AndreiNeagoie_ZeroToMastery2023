832645b019fb1299bb3da1cfcce75cfe3f685a0d
Anahita Akbarzadeh <akbarzadeh.annahita@gmail.com>
2023-12-06 19:13:05 +0330

    File Path in python

---
+++ 10.File-IO/File-Paths.py
@@ -0,0 +1,7 @@
+with open(
+    "/Users/anahita.akbarzadeh/Documents/Learning_Python_AndreiNeagoie_ZeroToMastery2023/10.File-IO/sad.txt",
+    mode="r",
+) as my_file:
+    print(my_file.read())
+
+# pathlib - Object oriented filesystem paths
